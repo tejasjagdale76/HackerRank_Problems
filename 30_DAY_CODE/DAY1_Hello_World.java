3d802d158b035d1207a213c66b59db47cf696925
tejasjagdale76 <tjagdale76@gmail.com>
2026-07-16 12:51:29 +0000

    DAY1 PUSH

--- 30_DAY_CODE/DAY1_Hello_World.java
+++ 30_DAY_CODE/DAY1_Hello_World.java
@@ -2,8 +2,6 @@
 // To complete this challenge, you must save a line of input from stdin to a variable,
 // print Hello, World. on a single line, and finally print the value of your variable on a second line.
 
-// You've got this!
-
 // Note: The instructions are Java-based, 
 // but we support submissions in many popular languages.
 //  You can switch languages using the drop-down menu above your editor, 
